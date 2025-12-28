8d30b26e700fd8fb2ae4b23101479ea53c301879
Vamshikorra <vamshichauhan2@gmail.com>
2025-12-28 13:55:58 +0530

    Add API endpoints to create and list tasks

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Task
+from .serializers import TaskSerializer
+
+
+class TaskListCreate(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
